4721fdd47b4508f112c9e83374647ef2780f6de6
Saied <elsaied.samaka1997@gmail.com>
2023-05-03 20:44:27 +0300

    wrap up

--- pages/views/Clients.js
+++ pages/views/Clients.js
@@ -2,7 +2,7 @@ const Clients = () => {
   return (
     <div className="w-full flex flex-col items-center bg-white rounded-md shadow-md mx-2 my-2">
       <h1>We are the champions</h1>
-      <div className="flex w-2/3 items-center justify-around">
+      <div className="flex w-2/3 items-center justify-around mb-5">
         <div>placeholder</div>
         <div>placeholder</div>
         <div>placeholder</div>
